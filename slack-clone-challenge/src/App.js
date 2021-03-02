3f88dd5aa5803c75cc0bb424a9e5b94c522237ec
Abhay <ap3919529@gmail.com>
2021-03-03 01:21:28 +0530

    docs: add router and swith to route
    Using Router to route throughout using swith and giving path to route to switch between chat and login page

--- slack-clone-challenge/src/App.js
+++ slack-clone-challenge/src/App.js
@@ -6,6 +6,27 @@ import  {BrowserRouter as Router , Route , Switch } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+    
+    
+
+    <Router>
+
+    <Switch>
+
+    <Route path="/room">
+      Chat
+    </Route>
+
+    <Route path="/">
+      Login
+    </Route>
+
+    </Switch>
+
+    </Router>
+
+   
+    
 
 
       
