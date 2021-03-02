6108ec19c1cce8043850af1003f8614493bc4fa1
Abhay <ap3919529@gmail.com>
2021-03-03 01:43:28 +0530

    import: chat and login components

--- slack-clone-challenge/src/App.js
+++ slack-clone-challenge/src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
 import  {BrowserRouter as Router , Route , Switch } from 'react-router-dom'
-  
+import Chat from "./components/chat";
+import Login from "./components/login";
 
 function App() {
   return (
