052b45b4ba5a952ff7a37aeb89ba89fe2a3a2e90
Abhay <ap3919529@gmail.com>
2021-03-03 04:18:13 +0530

    feat: importing accountbox icon

--- slack-clone-challenge/src/components/header.js
+++ slack-clone-challenge/src/components/header.js
@@ -1,40 +1,35 @@
 import React from "react";
-import styled from 'styled-components'
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import styled from "styled-components";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 function header() {
   return (
     <Container>
+      <Main>
+        <AccessTimeIcon />
 
-         <Main>
+        <input type="text" placeholder="Search..." />
 
-           < AccessTimeIcon />
+        <HelpOutlineIcon />
+      </Main>
 
-           <input type="text" placeholder="Search..." />
+      <UserContainer>
+          Abhay Patil
 
-           < HelpOutlineIcon />
+          
 
-
-         </Main>
-
-         {/* <UserContainer>
-
-         </UserContainer> */}
+      </UserContainer>
     </Container>
-  )
-  
+  );
 }
 
 export default header;
 
-
-
 const Container = styled.div`
-
-background:#924444;
-
-
+  background: #924444;
 `;
 
-
 const Main = styled.div``;
+
+const UserContainer = styled.div``;
