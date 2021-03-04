af228b3b2ec43ae8d573a06fba9e71ed63725070
Abhay <ap3919529@gmail.com>
2021-03-04 23:04:35 +0530

    docs: Create sidebar items as database

---
+++ slack-clone-challenge/src/data/Sidebardata.js
@@ -0,0 +1,46 @@
+
+import AppsIcon from '@material-ui/icons/Apps';
+import PeopleIcon from '@material-ui/icons/People';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import InboxIcon from '@material-ui/icons/Inbox';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+
+export const SidebarItems = [
+    {
+
+        icon: <ChatBubbleOutlineIcon/>,
+        text: "Thread"
+       
+    },
+    {
+
+        icon: <InboxIcon/>,
+        text: "All DMs"
+       
+    },
+    {
+
+        icon:  <DraftsIcon/>,
+        text: "Mentions & Reactions"
+       
+    },
+    {
+
+        icon:  <BookmarkBorderIcon/>,
+        text: "Save items"
+       
+    },
+    {
+
+        icon: <PeopleIcon/>,
+        text: "People & Groups"
+       
+    },
+    {
+
+        icon:  <AppsIcon/>,
+        text: "More"
+       
+    }
+]
