8db2cb3e9601b172c52f143545620ee607a577d1
Abhay <ap3919529@gmail.com>
2021-03-05 01:40:06 +0530

    docs: Import GlobalStyle
    We defined a GlobalStyle component and assigned background and color properties to values from the theme object.

--- slack-clone-challenge/src/components/GlobalStyles.js
+++ slack-clone-challenge/src/components/GlobalStyles.js
@@ -1,2 +1,9 @@
 
 import {createGlobalStyle} from "styled-components"
+
+export const GlobalStyles = createGlobalStyle `
+body {
+    background: ${({ theme }) => theme.body } ;
+    color: ${({theme}) => theme.text};
+    transitoion: all 0.50s linear;
+}`
