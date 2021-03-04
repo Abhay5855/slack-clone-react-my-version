4263c7f5b3ed49f5459a827854c4ef28f6398f58
Abhay <ap3919529@gmail.com>
2021-03-05 03:30:40 +0530

    docs: Import icons and display items as flex

--- slack-clone-challenge/src/components/chat.js
+++ slack-clone-challenge/src/components/chat.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import InfoIcon from "@material-ui/icons/Info";
 function chat() {
-    return (
-        <Container>
+  return (
+    <Container>
+      <ChatHeader>
+        <Title>
+          AbhayPatil
+          <StarBorderIcon />
+        </Title>
+      </ChatHeader>
 
-          
-          <ChatHeader>
-              <Title>
-                  AbhayPatil
-              </Title>
-          </ChatHeader>
-
-
-
-
-
-
-
-        
-            
-        </Container>
-    )
+      <DetailsHeader>
+        <Details>
+          Details
+          <InfoIcon />
+        </Details>
+      </DetailsHeader>
+    </Container>
+  );
 }
 
-export default chat
+export default chat;
 
-
-
-const Container = styled.div``;
-const ChatHeader = styled.div``;
+const Container = styled.div`
+display:flex;`;
+const ChatHeader = styled.div`
+`;
 const Title = styled.div``;
-
+const DetailsHeader = styled.div``;
+const Details = styled.div``;
