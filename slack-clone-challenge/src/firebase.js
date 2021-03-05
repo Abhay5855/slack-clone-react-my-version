73a278d43446a482b445eebaace4f53ae6d31ab3
Abhay <ap3919529@gmail.com>
2021-03-05 07:54:02 +0530

    docs: Initialize firebase App as db

---
+++ slack-clone-challenge/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from  'firebase'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCK9xbOPV5TFSkqGQRH1TG0VusPhxavph8",
+    authDomain: "slack-clone-react-63ec9.firebaseapp.com",
+    projectId: "slack-clone-react-63ec9",
+    storageBucket: "slack-clone-react-63ec9.appspot.com",
+    messagingSenderId: "1082951288669",
+    appId: "1:1082951288669:web:9f37245b3d776934238ab4"
+  };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
