9d0e384b0af9a12d90b53e9873fa2e10acbaf28c
Abhay <ap3919529@gmail.com>
2021-03-07 00:47:25 +0530

    docs: Creating three styled components one for image , other for Name and date and finally for message

--- slack-clone-challenge/src/components/ChatMessage.js
+++ slack-clone-challenge/src/components/ChatMessage.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 function ChatMessage() {
-    return (
-        <Container>
+  return (
+    <Container>
+      <UserImage>
+        <img
+          src="https://randomuser.me/api/portraits/women/29.jpg"
+          alt="lady"
+        />
+      </UserImage>
 
+      <MessageDetails>
+        <Name>
+          Alice Hao
+          <span>06/03/2021 12:35:56 AM</span>
+        </Name>
 
-        <UserImage>
-            <img src="https://randomuser.me/api/portraits/women/29.jpg" alt="lady" />
-
-            
-        </UserImage>
-
-
-            
-        </Container>
-    )
+        <Text>Hi! Your sketches are just awesome.</Text>
+      </MessageDetails>
+    </Container>
+  );
 }
 
-export default ChatMessage
-
+export default ChatMessage;
 
 const Container = styled.div``;
 const UserImage = styled.div``;
+const MessageDetails = styled.div``;
+const Name = styled.div``;
+const Text = styled.div``;
