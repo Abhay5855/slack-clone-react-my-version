e21b710cd748570da4600b277ac73632bf2bb996
Abhay <ap3919529@gmail.com>
2021-03-07 00:20:17 +0530

    docs: Create a new styled component for message

---
+++ slack-clone-challenge/src/components/ChatMessage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+function ChatMessage() {
+    return (
+        <Container>
+
+
+        <UserImage>
+            <img src="https://randomuser.me/api/portraits/women/29.jpg" alt="lady" />
+
+            
+        </UserImage>
+
+
+            
+        </Container>
+    )
+}
+
+export default ChatMessage
+
+
+const Container = styled.div``;
+const UserImage = styled.div``;
