2096a7b264418153bed9b7819910b476f67b4a29
Abhay <ap3919529@gmail.com>
2021-03-07 01:31:40 +0530

    style: Chatmessage container

--- slack-clone-challenge/src/components/ChatMessage.js
+++ slack-clone-challenge/src/components/ChatMessage.js
@@ -24,8 +24,51 @@ function ChatMessage() {
 
 export default ChatMessage;
 
-const Container = styled.div``;
-const UserImage = styled.div``;
-const MessageDetails = styled.div``;
-const Name = styled.div``;
+const Container = styled.div`
+
+padding:15px 2px 8px 15px;
+display:flex;
+align-items:center;`;
+
+
+
+const UserImage = styled.div`
+width:40px;
+height:40px;
+border-radius: 2px;
+overflow:hidden;
+margin-right:8px;
+img {
+    width:100%;
+}`;
+
+
+
+
+
+
+const MessageDetails = styled.div`
+padding-left:5px;
+display:flex;
+flex-direction:column;
+
+`;
+
+
+
+const Name = styled.span`
+font-weight:900;
+font-size:20px;
+line-height:1.3;
+
+span {
+    font-size:16px;
+    margin-left:8px;
+    font-weight:400px;
+    color:rgb(97,96,97);
+}`;
+
+
+
+
 const Text = styled.div``;
