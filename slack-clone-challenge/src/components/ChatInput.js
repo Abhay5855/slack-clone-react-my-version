e23978e98ad079ab32da7539c4daaef5a72bc111
Abhay <ap3919529@gmail.com>
2021-03-06 22:53:46 +0530

    docs: Create a seperate chat Input component

---
+++ slack-clone-challenge/src/components/ChatInput.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import styled from 'styled-components'
+
+function ChatInput() {
+    return (
+        <Container>
+            Chat Input
+        </Container>
+    )
+}
+
+export default ChatInput
+
+
+const Container = styled.div``;
