2462a93b08b2654ac55d18baecc1fae9a5cdc989
Abhay <ap3919529@gmail.com>
2021-03-07 09:17:55 +0530

    feat: Using Firebase auth and provider

--- slack-clone-challenge/src/firebase.js
+++ slack-clone-challenge/src/firebase.js
@@ -15,6 +15,13 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+
+
+export {auth,provider}
 
 
 export  default db;
