d724f43480d63e1b0dab2ae312c9481cec2d0bd7
Abhay <ap3919529@gmail.com>
2021-03-07 09:07:31 +0530

    style: Slack image and signIn button

--- slack-clone-challenge/src/components/login.js
+++ slack-clone-challenge/src/components/login.js
@@ -8,6 +8,17 @@ function login() {
 
         <SlackImage src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"/>
 
+          <h1>Sign in Slack</h1>
+
+
+           <SigninButton>
+
+          Sign In With Google
+
+           </SigninButton>
+
+
+
         </Contents>
 
         
@@ -22,10 +33,48 @@ export default login
 const Container = styled.div`
 background:#E5E7EB;
 width:100%;
-height:100vh;`;
+height:100vh;
+display:flex;
+align-items:center;
+justify-content:center;`;
 
 
-const Contents = styled.div``;
+const Contents = styled.div`
+background:white;
+padding:100px;
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+border-radius:5px;
+box-shadow:0 1px 3px rgb(0 0 0 /12%), 0 1px 2px rgb(0 0 0 / 24%);
 
 
-const SlackImage = styled.image``;
+
+
+`;
+
+
+const SlackImage = styled.img`
+height:100px;`;
+
+
+const SigninButton = styled.button`
+
+margin-top:50px;
+background:#059669;
+border:none;
+cursor:pointer;
+border-radius:2px;
+color:white;
+height:40px;
+font-size:15px;
+
+
+:hover{
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+`;
+
+
